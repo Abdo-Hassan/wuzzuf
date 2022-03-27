91bfcd0262fe9d80c22b8026e9482d2615ce2fa4
abdo <abdoholmes97@gmail.com>
2022-03-27 03:25:04 +0200

    refactor: disable redux devtools

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { jobsReducer } from './redux/reducer/jobsReducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
-  }
-}
+// declare global {
+//   interface Window {
+//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
+//   }
+// }
 
 const persistConfig = {
   key: 'root',
@@ -16,11 +16,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, jobsReducer);
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const store = createStore(
-  persistedReducer,
-  compose(applyMiddleware(), composeEnhancers())
-);
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(persistedReducer, applyMiddleware());
 
 const persistor = persistStore(store);
 export { persistor };
